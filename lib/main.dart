ed8990ba020aa37fbabd3d56cfce580dec56c985
Femi Tobi <adefemioluwatobi13@gmail.com>
2025-06-28 23:30:45 +0000

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,26 @@ void main() {
       appBar: AppBar(
         title: Text('My first app'),
         centerTitle: true,
+        backgroundColor: Colors.red,
       ),
+      body: Center(
+        child: Text(
+            'hello world',
+                style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                  color: Colors.grey,
+        ),
+        ),
+      ),
+        floatingActionButton: FloatingActionButton(
+    onPressed: () {
+      print('you pressed a button');
+      },
+        child: Text('hello'),
+          backgroundColor: Colors.red,
+    ),
     )
   ));
 }
